4af8c976222bcd354e17b42a45d104e46639a7b0
Sunil Pai <threepointone@gmail.com>
2015-04-28 14:51:41 +0530

    spaces

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
 import csp, {go, timeout, chan, putAsync} from 'js-csp';
 
 export function never(){
-	var c = chan();
-	c.close();
-	return c;
+  var c = chan();
+  c.close();
+  return c;
 }
 
 export function later(wait, value){
-	var c = chan();
-	setTimeout(()=>{
-		putAsync(c, value);
-		c.close();
-	}, wait);
-	return c;
+  var c = chan();
+  setTimeout(()=>{
+    putAsync(c, value);
+    c.close();
+  }, wait);
+  return c;
 }
 
 // todo - clearInterval?
 export function interval(interval, value){
-	var c = chan();
-	setInterval(()=>{
-		putAsync(c, value);
-	}, interval);
-	return c;	
+  var c = chan();
+  setInterval(()=>{
+    putAsync(c, value);
+  }, interval);
+  return c;  
 }
 
 export function sequentially(interval, values){
-	var c = chan();
-	go(function*(){
-		for(let value of values){
-			yield timeout(interval);
-			putAsync(c, value);
-		}
-		c.close();
-	})
-	return c;
+  var c = chan();
+  go(function*(){
+    for(let value of values){
+      yield timeout(interval);
+      putAsync(c, value);
+    }
+    c.close();
+  })
+  return c;
 }
 
 export function fromPoll(interval, fn){
-	var c = chan();
-	setInterval(()=>{
-		putAsync(c, fn());
-	}, interval);
-	return c;	
+  var c = chan();
+  setInterval(()=>{
+    putAsync(c, fn());
+  }, interval);
+  return c;  
 }
 
 export function withInterval(interval, handler){
-	var c = chan();
-	setInterval(()=>{
-		handler(c);
-	}, interval);
-	return c;
+  var c = chan();
+  setInterval(()=>{
+    handler(c);
+  }, interval);
+  return c;
 }
 
 export function fromCallback(fn){
-	var c = chan();
-	fn(function(value){
-		putAsync(c, value);
-		c.close();
-	})
-	return c;
+  var c = chan();
+  fn(function(value){
+    putAsync(c, value);
+    c.close();
+  })
+  return c;
 }
 
 export function fromNodeCallback(fn){
-	var c = chan();
-	fn(function(err, res){
-		putAsync(c, err || res);
-		c.close();
-	})
-	return c;
+  var c = chan();
+  fn(function(err, res){
+    putAsync(c, err || res);
+    c.close();
+  })
+  return c;
 }
 
 // todo - how to unbind?
 export function fromEvents(emitter, event){
-	var c = chan();
-	var fn = (...args) => putAsync(c, args)
-	emitter.on(event, fn);	
-	return c;
+  var c = chan();
+  var fn = (...args) => putAsync(c, args)
+  emitter.on(event, fn);  
+  return c;
 }
 
 // Create a property
--- test/index.js
+++ test/index.js
@@ -5,94 +5,94 @@ import csp, {go, timeout, chan, putAsync} from 'js-csp';
 import {EventEmitter} from 'events';
 
 import {
-	later,
-	sequentially,
-	interval,
-	fromPoll,
-	withInterval,
-	fromCallback,
-	fromNodeCallback,
-	fromEvents
+  later,
+  sequentially,
+  interval,
+  fromPoll,
+  withInterval,
+  fromCallback,
+  fromNodeCallback,
+  fromEvents
 } from '../index.js';
 
 let {into, take, reduce} = csp.operations;
 
 function now(){
-	return new Date().getTime()
+  return new Date().getTime()
 }
 
 describe('create', ()=>{
-	it('later', done => go(function*(){
-		var start = now();
-		(yield later(1000, 25)).should.eql(25);
-		((now() - start) > 1000).should.be.ok;
-		done();
-	}))
+  it('later', done => go(function*(){
+    var start = now();
+    (yield later(1000, 25)).should.eql(25);
+    ((now() - start) > 1000).should.be.ok;
+    done();
+  }))
 
-	it('interval', done => go(function*(){
-		var start = now();
-		(yield reduce((a,b)=> a+b, 0, take(5, interval(200, 5)))).should.eql(25);
-		((now() - start) > 1000).should.be.ok;
-		done();
-	}))
+  it('interval', done => go(function*(){
+    var start = now();
+    (yield reduce((a,b)=> a+b, 0, take(5, interval(200, 5)))).should.eql(25);
+    ((now() - start) > 1000).should.be.ok;
+    done();
+  }))
 
-	it('sequentially', done => go(function*(){
-		var start = now();
-		(yield into([], sequentially(200, ['a', 'b', 'c', 'd', 'e'])))
-			.should.eql(['a', 'b', 'c', 'd', 'e']);
-		((now() - start) > 1000).should.be.ok;
-		done();
-	}))
+  it('sequentially', done => go(function*(){
+    var start = now();
+    (yield into([], sequentially(200, ['a', 'b', 'c', 'd', 'e'])))
+      .should.eql(['a', 'b', 'c', 'd', 'e']);
+    ((now() - start) > 1000).should.be.ok;
+    done();
+  }))
 
 
-	it('fromPoll', done => go(function*(){
-		var start = now();
-		var i = 0;
-		(yield reduce((a,b) => a+b, 0, take(5, fromPoll(200, ()=> i++)))).should.eql(10);
-		((now() - start) > 1000).should.be.ok;
-		done();
-	}))
+  it('fromPoll', done => go(function*(){
+    var start = now();
+    var i = 0;
+    (yield reduce((a,b) => a+b, 0, take(5, fromPoll(200, ()=> i++)))).should.eql(10);
+    ((now() - start) > 1000).should.be.ok;
+    done();
+  }))
 
-	it('withInterval', done => go(function*(){
-		var start = now();
-		(yield into([], take(5, withInterval(200, ch => putAsync(ch, 5))))).should.eql([5, 5, 5, 5, 5]);
-		((now() - start) > 1000).should.be.ok;
-		done();
-	}))
+  it('withInterval', done => go(function*(){
+    var start = now();
+    (yield into([], take(5, withInterval(200, ch => putAsync(ch, 5))))).should.eql([5, 5, 5, 5, 5]);
+    ((now() - start) > 1000).should.be.ok;
+    done();
+  }))
 
-	it('fromCallback', done => go(function*(){
-		var start = now();
-		(yield fromCallback(function(fn){
-					setTimeout(()=> fn(5), 200);
-				})).should.eql(5);
-		((now() - start) >= 200).should.be.ok;
-		done();
-	}))
+  it('fromCallback', done => go(function*(){
+    var start = now();
+    (yield fromCallback(function(fn){
+          setTimeout(()=> fn(5), 200);
+        })).should.eql(5);
+    ((now() - start) >= 200).should.be.ok;
+    done();
+  }))
 
-	it('fromNodeCallback', done => go(function*(){
-		var start = now();
-		(yield fromNodeCallback(function(fn){
-					setTimeout(()=> fn(new Error('some error')), 200);
-				})).should.be.an.Error;
+  it('fromNodeCallback', done => go(function*(){
+    var start = now();
+    (yield fromNodeCallback(function(fn){
+          setTimeout(()=> fn(new Error('some error')), 200);
+        })).should.be.an.Error;
 
-		(yield fromNodeCallback(function(fn){
-					setTimeout(()=> fn(null, 'value'), 200);
-				})).should.eql('value');
-		((now() - start) > 200).should.be.ok;
-		done();
+    (yield fromNodeCallback(function(fn){
+          setTimeout(()=> fn(null, 'value'), 200);
+        })).should.eql('value');
+    ((now() - start) > 200).should.be.ok;
+    done();
 
-	}))
+  }))
 
-	it('fromEvents', done => go(function*(){
-		var emitter = new EventEmitter();
-		var ch = fromEvents(emitter, 'beep');
-		for(var i=0; i<20; i++){
-			emitter.emit('beep', 1);
-		}
-		(yield into([], take(5, ch))).map(arr => arr[0]).should.eql([1, 1, 1, 1, 1]);
-		done();		
-	}))
+  it('fromEvents', done => go(function*(){
+    var emitter = new EventEmitter();
+    var ch = fromEvents(emitter, 'beep');
+    for(var i=0; i<20; i++){
+      emitter.emit('beep', 1);
+    }
+    (yield into([], take(5, ch))).map(arr => arr[0]).should.eql([1, 1, 1, 1, 1]);
+    done();    
+  }))
 
-	
+  
 });
 
